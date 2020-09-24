dc9182883bfa94d8e5dc4ef8cef00758ae893ce6
deccer <decccer@outlook.com>
2020-09-24 23:26:38 +0400

    Move Extensions up in the folder hierarchy

---
+++ UOLandscape/Extensions/NumericsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace UOLandscape.Extensions
+{
+    public static class NumericsExtensions
+    {
+        public static Vector3 ToXnaVector3(this System.Numerics.Vector3 value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+
+        public static Vector2 ToXnaVector2(this System.Numerics.Vector2 value)
+        {
+            return new Vector2(value.X, value.Y);
+        }
+    }
+}
--- UOLandscape/Extensions/VectorExtensions.cs
+++ UOLandscape/Extensions/VectorExtensions.cs
@@ -3,20 +3,10 @@ using NumVector3 = System.Numerics.Vector3;
 using XnaVector2 = Microsoft.Xna.Framework.Vector2;
 using XnaVector3 = Microsoft.Xna.Framework.Vector3;
 
-namespace UOLandscape.UI.Extensions
+namespace UOLandscape.Extensions
 {
-    public static class DataConvertExtensions
+    public static class VectorExtensions
     {
-        public static XnaVector3 ToXnaVector3(this NumVector3 value)
-        {
-            return new XnaVector3(value.X, value.Y, value.Z);
-        }
-
-        public static XnaVector2 ToXnaVector2(this NumVector2 value)
-        {
-            return new XnaVector2(value.X, value.Y);
-        }
-
         public static NumVector3 ToNumericVector3(this XnaVector3 value)
         {
             return new NumVector3(value.X, value.Y, value.Z);
